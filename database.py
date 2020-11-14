1077afc96dabca61075d6a3c1831529cbfb5292f
Jasmine Latendresse <latendresse.jasmine@gmail.com>
2020-11-14 13:11:14 -0500

    dumped json file into local sqlite db

--- database.py
+++ database.py
@@ -1,5 +1,7 @@
 import sqlite3
 from sqlite3 import Error
+import json
+
 
 def create_connection(db_file):
     """ create a database connection to the SQLite database
@@ -16,6 +18,7 @@ def create_connection(db_file):
 
     return conn
 
+
 def create_table(conn, create_table_sql):
     """ create a table from the create_table_sql statement
     :param conn: Connection object
@@ -28,37 +31,45 @@ def create_table(conn, create_table_sql):
     except Error as e:
         print(e)
 
+
 def main():
     database = r"../sqlite/db/sstubs.db"
+    
+    with open('../MSR/sstubs.json') as json_file:
+        json_data = json.loads(json_file.read())
 
-    sql_create_commits_table = """ CREATE TABLE IF NOT EXISTS commits (
-                                        id integer PRIMARY KEY,
-                                        bug_type text NOT NULL, 
-                                        fix_commit_sha1 text NOT NULL,
-                                        fix_commit_parent_sha1 text NOT NULL, 
-                                        bug_file_path text,
-                                        fix_patch text, 
-                                        project_name text,
-                                        bug_line_number integer, 
-                                        bug_node_start_char integer, 
-                                        bug_node_length integer, 
-                                        fix_line_number integer, 
-                                        fix_node_start_char integer, 
-                                        fix_node_length integer, 
-                                        source_before_fix text, 
-                                        source_after_fix text
-                                    ); """
+        columns = []
+        column = []
+        for data in json_data:
+                column = list(data.keys())
+                for col in column:
+                    if col not in columns:
+                        columns.append(col)
 
-    # create a database connection
-    conn = create_connection(database)
+        value = []
+        values = []
+        for data in json_data:
+                for i in columns:
+                    value.append(str(dict(data).get(i)))
+                values.append(list(value))
+                value.clear()
 
-    # create tables
-    if conn is not None:
-        # create projects table
-        create_table(conn, sql_create_commits_table)
+        sql_create_commits_table = "create table if not exists commits ({0})".format(" text,".join(columns))
 
-    else:
-        print("Error! cannot create the database connection.")
+        sql_insert_commits = "insert into commits ({0}) values (?{1})".format(",".join(columns), ",?" * (len(columns)-1))
+        # create a database connection
+        conn = create_connection(database)
+        c = conn.cursor()
+        # create tables
+        if conn is not None:
+            c.execute(sql_create_commits_table)
+            c.executemany(sql_insert_commits, values)
+            values.clear()
+            conn.commit()
+            conn.close()
+
+        else:
+            print("Error! cannot create the database connection.")
 
 
 if __name__ == '__main__':
