0e908a9a4b7bea3af38827e889fc1153a8e7a8c2
Jasmine Latendresse <latendresse.jasmine@gmail.com>
2020-12-12 12:51:06 -0500

    added travis torrent project names table to the database

--- database.py
+++ database.py
@@ -31,11 +31,8 @@ def create_table(conn, create_table_sql):
     except Error as e:
         print(e)
 
-
-def main():
-    database = r"../sqlite/db/sstubs.db"
-    
-    with open('docs/sstubs.json') as json_file:
+def create_commits_table(commits_json, database):
+    with open(commits_json) as json_file:
         json_data = json.loads(json_file.read())
 
         columns = []
@@ -71,6 +68,49 @@ def main():
         else:
             print("Error! cannot create the database connection.")
 
+def create_travistorrent_names_table(names_json, database):
+    with open(names_json) as json_file:
+        json_data = json.loads(json_file.read())
+
+        columns = []
+        column = []
+        for data in json_data:
+                column = list(data.keys())
+                for col in column:
+                    if col not in columns:
+                        columns.append(col)
+
+        value = []
+        values = []
+        for data in json_data:
+                for i in columns:
+                    value.append(str(dict(data).get(i)))
+                values.append(list(value))
+                value.clear()
+
+        sql_create_commits_table = "create table if not exists names ({0})".format(" text,".join(columns))
+
+        sql_insert_commits = "insert into names ({0}) values (?{1})".format(",".join(columns), ",?" * (len(columns)-1))
+        # create a database connection
+        conn = create_connection(database)
+        c = conn.cursor()
+        # create tables
+        if conn is not None:
+            c.execute(sql_create_commits_table)
+            c.executemany(sql_insert_commits, values)
+            values.clear()
+            conn.commit()
+            conn.close()
+
+        else:
+            print("Error! cannot create the database connection.")
+
+def main():
+    database = r"../sqlite/db/sstubs.db"
+    commits_file = 'docs/sstubs.json'
+    names_file = 'docs/travistorrent_project_names.json'
+    create_commits_table(commits_file, database)
+    create_travistorrent_names_table(names_file, database)
 
 if __name__ == '__main__':
     main()
