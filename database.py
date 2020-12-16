656f18b7d39062cc77c132e615650d6fabac0058
Jasmine Latendresse <latendresse.jasmine@gmail.com>
2020-12-16 11:46:40 -0500

    travis torrent db created in sqlite

--- database.py
+++ database.py
@@ -68,6 +68,43 @@ def create_commits_table(commits_json, database):
         else:
             print("Error! cannot create the database connection.")
 
+def create_travis_torrent_table(merged_travis, database):
+    with open(merged_travis) as json_file:
+        json_data = json.loads(json_file.read())
+
+        columns = []
+        column = []
+        for data in json_data:
+                column = list(data.keys())
+                for col in column:
+                    if col not in columns:
+                        columns.append(col)
+
+        value = []
+        values = []
+        for data in json_data:
+                for i in columns:
+                    value.append(str(dict(data).get(i)))
+                values.append(list(value))
+                value.clear()
+
+        sql_create_travis_table = "create table if not exists travis ({0})".format(" text,".join(columns))
+
+        sql_insert_travis = "insert into travis ({0}) values (?{1})".format(",".join(columns), ",?" * (len(columns)-1))
+        # create a database connection
+        conn = create_connection(database)
+        c = conn.cursor()
+        # create tables
+        if conn is not None:
+            c.execute(sql_create_travis_table)
+            c.executemany(sql_insert_travis, values)
+            values.clear()
+            conn.commit()
+            conn.close()
+
+        else:
+            print("Error! cannot create the database connection.")
+
 def create_travistorrent_names_table(names_json, database):
     with open(names_json) as json_file:
         json_data = json.loads(json_file.read())
@@ -109,8 +146,10 @@ def main():
     database = r"../sqlite/db/sstubs.db"
     commits_file = 'docs/sstubs.json'
     names_file = 'docs/travistorrent_project_names.json'
+    travis_file = 'docs/merged_travis.json'
     create_commits_table(commits_file, database)
     create_travistorrent_names_table(names_file, database)
+    create_travis_torrent_table(travis_file, database)
 
 if __name__ == '__main__':
     main()
