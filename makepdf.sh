503fc3615d29521fe82f7157c65321f477e2c9ff
jaslatendresse <latendresse.jasmine@gmail.com>
2020-12-29 11:53:23 -0500

    acm style pipeline

---
+++ makepdf.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# docker build -t pandoc-eisvogel-acm ./docker-msr
+
+document=msr-challenge-2020
+
+docker run \
+  --rm \
+  -v $GITHUB_WORKSPACE/$document:/project \
+  -w /project \
+  --entrypoint "./makepdf.sh" \
+  pandoc-eisvogel-acm \
+  $document
