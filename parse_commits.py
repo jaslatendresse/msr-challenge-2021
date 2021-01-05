213c0b996a53b39e47975bfce9a105c6ff5b7e63
Jasmine Latendresse <latendresse.jasmine@gmail.com>
2021-01-05 12:07:14 -0500

    comparing non-pr commits with commits in push list

--- parse_commits.py
+++ parse_commits.py
@@ -22,9 +22,7 @@ def create_connection(db_file):
 
     return conn
 
-def create_list(database, table_name, column_name):
-    query = 'SELECT ' + column_name + ' FROM ' + table_name
-
+def create_commit_list(database, query, text_file):
     conn = create_connection(database)
     c = conn.cursor()
 
@@ -32,22 +30,57 @@ def create_list(database, table_name, column_name):
             c.execute(query)
             results = c.fetchall()
             final_result = [i[0] for i in results]
-            with open('docs/gh_commits_in_push.txt', 'w') as f:
+            with open(text_file, 'w') as f:
                 for row in final_result:
-                    if row.strip('\n') != 'None':
-                        f.write("%s\n" % str(row))
+                    f.write("%s\n" % str(row))
             conn.commit()
             conn.close()
 
     else:
         print("Error! cannot create the database connection.")
 
-#def parse_list(text_file):
+def create_list(database, query, text_file):
+    conn = create_connection(database)
+    c = conn.cursor()
 
+    if conn is not None:
+            c.execute(query)
+            results = c.fetchall()
+            final_result = [i[0] for i in results]
+            with open(text_file, 'w') as f:
+                for row in final_result:
+                    if row.strip('\n') != 'None':
+                        f.write("%s\n" % str(row).split('#'))
+            conn.commit()
+            conn.close()
+
+    else:
+        print("Error! cannot create the database connection.")
+
+def parse_commits(text_file1, text_file2):
+    commits_list = []
+    with open(text_file1, 'r') as file1:
+        with open(text_file2, 'r') as file2: 
+            commits = [line.strip() for line in file1]
+            lines = [line.strip() for line in file2]
+            for commit in commits:
+                for line in lines:
+                    if str(commit) in line: 
+                        commits_list.append(commit)
+    #print(count)
+    final_list = list(dict.fromkeys(commits_list))
+    print(len(final_list))
 
 def main():
     database = r"../sqlite/db/msr_db.db"
-    create_list(database, 'selected_travis', 'gh_commits_in_push')
+    
+    gh_commits_query = 'SELECT gh_commits_in_push FROM selected_travis GROUP BY gh_commits_in_push'
+    create_list(database, gh_commits_query, 'docs/gh_commits_in_push.txt')
 
+    not_pr_query = 'SELECT fixCommitSha1 FROM (SELECT * FROM selected_sstubs LEFT JOIN selected_travis WHERE fixCommitSha1 = git_trigger_commit GROUP BY fixCommitSha1) WHERE gh_is_pr = "False"'
+    create_commit_list(database, not_pr_query, 'docs/not_a_pr_commit.txt')
+
+    parse_commits('docs/not_a_pr_commit.txt', 'docs/gh_commits_in_push.txt')
+    
 if __name__ == '__main__':
     main()
