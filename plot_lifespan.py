c10bbc992458716c54cbfa828d243fd6b2686bea
Diego Costa <diegoelias1@gmail.com>
2021-01-26 12:28:46 -0500

    Updating the plots to consider all SStuBs

--- plot_lifespan.py
+++ plot_lifespan.py
@@ -23,12 +23,22 @@ sns.displot(np.log10(1 + lifespan_df['duration (days)']))
 sns.set_context('paper', font_scale=1.2)
 
 # Because we don't have enough data, it is better to just bin the data and use simple bar plots
-to_plot = pd.cut(lifespan_df['duration (days)'], bins=[0,1, 7, 183, 365, 1000]).value_counts()
+to_plot_per_bug = pd.cut(lifespan_df['duration (days)'], bins=[0,1, 7, 183, 365, 1000]).value_counts()
 
-ax = sns.barplot(to_plot, y=to_plot.index, x=to_plot, palette='Blues')
+ax = sns.barplot(to_plot_per_bug, y=to_plot_per_bug.index, x=to_plot_per_bug, palette='Blues')
 
 ax.set_xlabel('# of SSBugs')
 ax.set_yticklabels(['less than 1 day', '2 - 7 days', '2 week - 6 months', '6 months - 1 year', '> 1 year'])
+
+
+n_bugs = len(lifespan_df)
+# Annotate bars
+for p in ax.patches:
+    width = p.get_width()
+    perc = width / n_bugs * 100.0
+    plt.text(15 + p.get_width(), p.get_y() + 0.55*p.get_height(),
+             '{:1.2f}%'.format(perc),
+             ha='center', va='center')
 
 sns.despine()
 
@@ -36,5 +46,29 @@ plt.tight_layout()
 plt.savefig('plots/lifespan_bar.pdf')
 
 # %%
+lifespan_commit_df = lifespan_df.drop_duplicates(subset=['bug_hash', 'fix_hash'])
+
+to_plot_per_commit = pd.cut(lifespan_commit_df['duration (days)'], bins=[0,1, 7, 183, 365, 1000]).value_counts()
+
+ax = sns.barplot(to_plot_per_commit, y=to_plot_per_commit.index, x=to_plot_per_commit, palette='Blues')
+
+ax.set_xlabel('# of Commits')
+ax.set_yticklabels(['less than 1 day', '2 - 7 days', '2 week - 6 months', '6 months - 1 year', '> 1 year'])
+
+
+n_bugs = len(lifespan_commit_df)
+# Annotate bars
+for p in ax.patches:
+    width = p.get_width()
+    perc = width / n_bugs * 100.0
+    plt.text(10 + p.get_width(), p.get_y() + 0.55*p.get_height(),
+             '{:1.2f}%'.format(perc),
+             ha='center', va='center')
+
+sns.despine()
+
+plt.tight_layout()
+plt.savefig('plots/lifespan_bar_commit.pdf')
+
 
 # %%
